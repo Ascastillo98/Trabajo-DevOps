399c6ce536b6a2532d8394a84a3e4a5b5a3e8f0e
Alejandro <alexguitarte@gmail.com>
2023-11-16 12:18:39 +0100

    Modificado proyecto

---
+++ matrix/src/main/java/es/uca/mps/matrix/App.java
@@ -0,0 +1,13 @@
+package es.uca.mps.matrix;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
